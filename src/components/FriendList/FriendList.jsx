786450d96a761e4350f15c7c75624be3fb8ae1ad
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2022-12-14 21:47:03 +0200

    final

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import { FriendListItem } from './FriendListItem';
-// import css from "./FriendList.module.css";
+import css from "./FriendList.module.css";
 
 export const FriendList = ({ friends }) => {
-    return (<ul>
+    return (<ul className={css.friendList}>
         {friends.map(friend => (
             <FriendListItem
                 key={friend.id}
